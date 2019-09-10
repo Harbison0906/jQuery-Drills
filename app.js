c315caa4f5e17fdec2f59e6df0be6d83b434c15b
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-10 18:36:10 -0500

    disable submit button until text is entered into the text box

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-$('#btnSubmit').click(function() {
+$(function () {
+  $('#text-field').keyup(function () {
+    if ($('#text-field').val().length > 0) {
+      $('#btnSubmit').removeAttr('disabled');
+    }
+  })
+
+});
+
+$('#btnSubmit').click(function () {
   let text = $('#text-field').val();
   alert(text);
 })
