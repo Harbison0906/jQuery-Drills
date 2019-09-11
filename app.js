d049606d049948e9503179666a4a565c5b5f6d8e
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2019-09-10 21:59:54 -0500

    appended div after the form

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ $(function () {
   $('#text-field').keyup(function () {
     if ($('#text-field').val().length > 0) {
       $('#btnSubmit').removeAttr('disabled');
+    } else {
+      $('#btnSubmit').addAttr('disbled');
     }
   })
 
 });
 
-$('#btnSubmit').click(function () {
+// $('#btnSubmit').click(function () {
   let text = $('#text-field').val();
-  alert(text);
-})
+// })
+
+$('form').after('<div id="box"></div>');
+$('div').css({
+  'background-color': 'gray',
+  'height': '100px',
+  'margin-top': '5px'
+});
+
+$('#btnSubmit').click(function() {
+  $('<h2></h2>').append(text);
+  $('#box').append('<h2></h2>');
+  
+});
